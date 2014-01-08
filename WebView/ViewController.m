6242dd3cd243f3f148cf2ac59f54ccd79f8ed2d0
steve0503 <leehr76@gmail.com>
2014-01-09 08:10:36 +0900

    WebView_4_4_5_pg_261

--- WebView/ViewController.m
+++ WebView/ViewController.m
@@ -8,11 +8,57 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITextFieldDelegate, UIWebViewDelegate>
+
+
 
 @end
 
 @implementation ViewController
+
+-(BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType{
+    
+    if(UIWebViewNavigationTypeLinkClicked == navigationType){
+        
+        NSLog(@"클릭금지");
+        
+        return NO;
+        
+    }
+    return YES;
+    
+}
+
+-(void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error{
+    
+    NSString *msg = [error localizedDescription];
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"에러" message:msg delegate:nil cancelButtonTitle:@"확인" otherButtonTitles:nil];
+    
+    [alert show];
+    
+    
+}
+
+-(BOOL)textFieldShouldReturn:(UITextField *)textField{
+    
+    [self go:nil];
+    
+    return YES;
+    
+}
+
+-(IBAction)go:(id)sender{
+    
+    if ([_textField.text length] > 3) {
+        NSURL *url = [NSURL URLWithString:_textField.text];
+        
+        NSURLRequest *request = [NSURLRequest requestWithURL:url];
+        [_webView loadReques:request];
+        
+        [_textField resignFirstResponder];
+    }
+    
+}
 
 - (void)viewDidLoad
 {
